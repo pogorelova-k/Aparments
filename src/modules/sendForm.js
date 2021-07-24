966dd451eef4eaaf05da5fa7aa0a629d97aac110
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-07-24 16:47:46 +0300

    fix send form

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,17 +1,8 @@
-// send-ajax-form
 const sendForm = () => {
-    const erorrMessage = 'Что-то пошло не так...',
-        successMesage = 'Запрос отправлен успешно',
-        loadMessage = `Загрузка...`,
-        url = './server.php';
-
     const form = document.querySelector('form'),
         inputs = form.querySelectorAll('input'),
-        sliders = form.querySelectorAll('.range__slider');
-
-    const statusMessage = document.createElement('div');
-    statusMessage.classList.add('status-mesage');
-
+        sliders = form.querySelectorAll('.range__slider'),
+        url = './server.php';
 
     const postData = body => fetch(url, {
         method: 'POST',
@@ -23,31 +14,11 @@ const sendForm = () => {
         body: JSON.stringify(body)
     });
 
-    // функция для отображения сообщения пользователю
-    const outputData = (time = 5000) => {
-        statusMessage.textContent = successMesage;
-        setTimeout(() => {
-            statusMessage.textContent = '';
-        }, time);
-    };
-
-    // функция для отображения ошибки пользователю и в консоль
-    const errorData = (error, time = 5000) => {
-        statusMessage.textContent = erorrMessage;
-        console.error(error);
-        setTimeout(() => {
-            statusMessage.textContent = '';
-        }, time);
-    };
-
     // sendForm
     const sendData = () => {
          // получаем значение из всех инпутов формы у которых есть атрибут name
         const formData = new FormData(form),
             body = {};
-
-        form.append(statusMessage);
-        statusMessage.innerHTML = loadMessage;
 
         // извлекаем данные из formData и добавляем в body
         formData.forEach((val, key) => {
@@ -59,9 +30,9 @@ const sendForm = () => {
                 if (response.status !== 200) {
                     throw new Error('status network not 200');
                 }
-                outputData();
+                // outputData();
             })
-            .catch(error => errorData(error))
+            .catch(error => console.warn(error))
     };
 
     sliders.forEach(slider => slider.addEventListener('click', () => sendData()));
